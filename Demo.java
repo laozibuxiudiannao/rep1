615e96223fcf96476bc9b00e06c2089e8c7901e3
liu <liu@126.com>
2019-10-29 17:10:05 +0800

    狗

--- Demo.java
+++ Demo.java
@@ -1,7 +1,10 @@
 package com.itheima.test;
 
+import javax.sound.midi.Soundbank;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("狗是人类的好朋友");
+        System.out.println("猫也是人类的好朋友");
     }
 }
